e7968ca2296fbbeece0a9e80badfc29c96072dc0
ecommerce <2dh2wdk2@naver.com>
2021-07-20 15:55:08 +0900

    210720

--- src/com/programmers/study/Failrate.java
+++ src/com/programmers/study/Failrate.java
@@ -3,10 +3,7 @@ package com.programmers.study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Failrate {
@@ -31,42 +28,44 @@ public class Failrate {
 	}
 
 	public static int[] solution(int N, int[] stages) {
-        int[] answer;
+        int[] answer = new int[N];
+        
         HashMap<Integer, Integer> stageMap = new HashMap<>();
         HashMap<Integer, Integer> stageRslt = new HashMap<>();
+        HashMap<Integer, Double> answerMap = new HashMap<>();
         
         for(int i=1;i<N+2;i++) {
         	stageMap.put(i, 0);
+        	stageRslt.put(i, 0);
+        	answerMap.put(i, (double) 0);
         }
-        
-        Arrays.sort(stages);
         
        	for(int stage : stages) {
        		for(int i=1;i<stage+1;i++) {
        			stageMap.put(i, stageMap.get(i)+1);
        		}
+       		stageRslt.put(stage, stageRslt.get(stage)+1);
        	}
        	
-       	for(int i=0;i<stages.length;) {
-       		int cnt = 1;
-       		int j = i+1;
-       		if(i!=stages.length-1) {
-       			while(stages[i]==stages[j]) {
-       				cnt++;
-       				j++;
+       	for(int stage : stages) {
+       		answerMap.put(stage, (double) stageRslt.get(stage)*100/stageMap.get(stage));
+       	}
+       	
+       	for(int i=0;i<N;i++) {
+       		if(i==0) {
+       			answer[i] = 1;
+       		} else {
+       			for(int j=i-1;j>=0;j--) {
+       				if(answerMap.get(i+1)>answerMap.get(answer[j])) {
+       					answer[j+1] = answer[j];
+       					answer[j] = i+1;
+       				} else {
+       					answer[j+1] = i+1;
+       					break;
+       				}
        			}
        		}
-       		stageRslt.put(stages[i], cnt*100/stageMap.get(stages[i]));
-       		i+=cnt;
        	}
-       	answer = new int[stageRslt.size()];
-       	Set<Entry<Integer, Integer>> entrySet = stageRslt.entrySet();
-
-       	for(Entry<Integer, Integer> x : entrySet) {
-       		System.out.print(x.getKey()+" ");
-       		System.out.println(x.getValue());
-       	}
-       	
        	return answer;
     }
 }
