b1669e90121b00ab7858900c92a7ae8a874d12a5
ecommerce <2dh2wdk2@naver.com>
2021-07-29 16:30:53 +0900

    210729

---
+++ src/com/programmers/study/NumberSum.java
@@ -0,0 +1,36 @@
+package com.programmers.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumberSum {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		System.out.println(solution(a, b));
+		
+		br.close();
+	}
+	
+	
+	public static long solution(int a, int b) {
+        long answer = 0;
+        
+        if(a>b) {
+        	for(int i=b;i<=a;i++) {
+        		answer += i;
+        	}
+        } else {
+        	for(int i=a;i<=b;i++) {
+        		answer += i;
+        	}
+        }
+        
+        return answer;
+    }
+}
