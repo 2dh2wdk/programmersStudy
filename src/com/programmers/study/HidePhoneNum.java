1ce33ff7c5f209ab16d99bd63a9ba34d899116fd
ecommerce <2dh2wdk2@naver.com>
2021-08-21 08:34:06 +0900

    210821

---
+++ src/com/programmers/study/HidePhoneNum.java
@@ -0,0 +1,29 @@
+package com.programmers.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HidePhoneNum {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		System.out.println(solution(br.readLine()));
+		
+		br.close();
+	}
+	
+	public static String solution(String phone_number) {
+        String answer = "";
+        
+        char[] phoneChar = phone_number.toCharArray();
+        
+        for(int i=0;i<phoneChar.length;i++) {
+        	if(i<phoneChar.length-4) answer+="*";
+        	else answer+=phoneChar[i];
+        }
+        
+        return answer;
+    }
+}
