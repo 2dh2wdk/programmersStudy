bedaf8cd756b271cd502816cb05a98a569a4ad7d
ecommerce <2dh2wdk2@naver.com>
2021-08-27 18:00:20 +0900

    210827

---
+++ src/com/programmers/study/StringCompression.java
@@ -0,0 +1,24 @@
+package com.programmers.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StringCompression {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(solution(br.readLine()));
+		
+		br.close();
+	}
+	
+	public static int solution(String s) {
+        int answer = 0;
+        
+        
+        
+        return answer;
+    }
+}
