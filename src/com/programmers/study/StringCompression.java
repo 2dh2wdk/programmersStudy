5f0ca5b82960ec0191a0ac7c265dcdf893a37d94
ecommerce <2dh2wdk2@naver.com>
2021-08-30 20:25:43 +0900

    210830

--- src/com/programmers/study/StringCompression.java
+++ src/com/programmers/study/StringCompression.java
@@ -17,7 +17,14 @@ public class StringCompression {
 	public static int solution(String s) {
         int answer = 0;
         
-        
+        for(int i=0;i<s.length()/2;i++) {
+        	for(int j=0;j<s.length();) {
+        		int idx=i;
+        		while(s.indexOf(j)==s.indexOf(j+idx)) {
+        			
+        		}
+        	}
+        }
         
         return answer;
     }
